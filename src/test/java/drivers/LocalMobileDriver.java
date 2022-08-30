6e81a8f853a34916c89229499dceda7b038f2537
Koten <Kotenkiton@gmail.com>
2022-08-30 19:49:07 +0300

    LocalMobileDriver added

---
+++ src/test/java/drivers/LocalMobileDriver.java
@@ -0,0 +1,42 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class LocalMobileDriver implements WebDriverProvider {
+
+
+    @Override
+    public WebDriver createDriver(Capabilities capabilities) {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.merge(capabilities);
+        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+        options.setPlatformName("Android");
+        options.setDeviceName("Pixel 3 API 33");
+        options.setPlatformVersion("13.0");
+        options.setApp("");
+        options.setAppPackage("");
+        options.setAppActivity("");
+
+        return new AndroidDriver(getAppiumServerUrl(), options);
+    }
+
+    public static URL getAppiumServerUrl() {
+        try {
+            return new URL("http://localhost:4723/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+}
+
+
+
