cd279a3336e944c99973b1df9ebce8ade8681c24
Koten <Kotenkiton@gmail.com>
2022-08-31 19:46:34 +0300

    Local mobile tested added ( don't work) notifications!!!

--- src/test/java/drivers/LocalMobileDriver.java
+++ src/test/java/drivers/LocalMobileDriver.java
@@ -7,23 +7,31 @@ import io.appium.java_client.remote.AutomationName;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 
 public class LocalMobileDriver implements WebDriverProvider {
 
 
     @Override
     public WebDriver createDriver(Capabilities capabilities) {
+
+        File app = getApp();
+
         UiAutomator2Options options = new UiAutomator2Options();
         options.merge(capabilities);
         options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
         options.setPlatformName("Android");
         options.setDeviceName("Pixel 3 API 33");
         options.setPlatformVersion("13.0");
-        options.setApp("");
-        options.setAppPackage("");
-        options.setAppActivity("");
+        options.setApp(app.getAbsolutePath());
+        options.setAppPackage("org.wikipedia.alpha");
+        options.setAppActivity("org.wikipedia.main.MainActivity");
 
         return new AndroidDriver(getAppiumServerUrl(), options);
     }
@@ -35,6 +43,22 @@ public class LocalMobileDriver implements WebDriverProvider {
             throw new RuntimeException(e);
 
         }
+    }
+
+    private File getApp() {
+        String appUrl = "https://github.com/wikimedia/apps-android-wikipedia/" +
+                "releases/download/latest/app-alpha-universal-release.apk";
+        String appPath = "src/test/resources/apps/app-alpha-universal-release.apk";
+
+        File app = new File(appPath);
+        if (!app.exists()) {
+            try (InputStream in = new URL(appUrl).openStream()) {
+                copyInputStreamToFile(in, app);
+            } catch (IOException e) {
+                throw new AssertionError("Failed to download application", e);
+            }
+        }
+        return app;
     }
 }
 
