252c5e1053aa1cc5b982dce77791d7e8dafd60e5
Koten <Kotenkiton@gmail.com>
2022-09-01 23:48:43 +0300

    added config

---
+++ src/test/java/owner/BrowserstackKeys.java
@@ -0,0 +1,30 @@
+package owner;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:config/local.properties"
+})
+public interface BrowserstackKeys extends Config {
+
+    String login();
+
+    String password();
+
+    String project();
+
+    String build();
+
+    String name();
+
+    String app();
+
+    String device();
+
+    String osVersion();
+
+    String baseUrl();
+
+}
