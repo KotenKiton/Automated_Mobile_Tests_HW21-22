7b3b811d217609313c8076625c34ba447e8ea726
Koten <Kotenkiton@gmail.com>
2022-09-02 23:51:33 +0300

    Local properties + locanConfig added

---
+++ src/test/java/config/LocalConfig.java
@@ -0,0 +1,18 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources("classpath:config/local.properties")
+public interface LocalConfig extends Config {
+    @DefaultValue("android")
+    String platformName();
+
+    @DefaultValue("Pixel 3 API 33")
+    String device();
+
+
+    @DefaultValue("http://localhost:4723/wd/hub")
+    String appURL();
+
+}
